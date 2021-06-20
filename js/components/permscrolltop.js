9c5ac583418045ec0ac648045077cec49a139cc7
Dexter Legaspi <1777690+dalegaspi@users.noreply.github.com>
2021-06-19 20:02:07 -0400

    dynamic appearing scroll to top

--- js/components/permscrolltop.js
+++ js/components/permscrolltop.js
@@ -3,14 +3,33 @@
  */
 
 export const SimplePermanentScrollTop = {
+    mounted() {
+        window.onscroll = () => this.onScroll();
+    },
     methods: {
+        onScroll() {
+            const SCROLL_THRESHOLD = 0.10;
+            let rootElement = document.documentElement;
+            let scrollTotal = rootElement.scrollHeight - rootElement.clientHeight
+            if ((rootElement.scrollTop / scrollTotal ) > SCROLL_THRESHOLD ) {
+                console.log('showing scroll to top button');
+                this.$refs.scrollToTop.style.visibility = 'visible';
+                this.$refs.scrollToTop.classList.remove('slide-out-right');
+                this.$refs.scrollToTop.classList.add('slide-in-right');
+            } else {
+                console.log('hiding scroll to top button');
+                this.$refs.scrollToTop.classList.remove('slide-in-right');
+                this.$refs.scrollToTop.classList.add('slide-out-right');
+            }
+        },
+
         scrollToTop(e) {
             e.preventDefault()
             window.scroll(0, 0);
         }
     },
     template: `
-        <button @click="scrollToTop" class="button-scroll-top">
+        <button ref="scrollToTop" @click="scrollToTop" class="button-scroll-top">
             <img src="images/arrow-up-outline-white.svg">
         </button>
     `
